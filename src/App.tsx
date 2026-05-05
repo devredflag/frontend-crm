3bf3abe6089af35234e2313be21cdd2cb891d16f
Gabriel Santos <gabrielsantos2411.gs@gmail.com>
2026-05-05 17:10:23 -0300

    feat: adicionar páginas de visualizar e editar empresa

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import NovaEmpresa from "./pages/empresas/nova";
 import Clientes from "./pages/clientes";
+import EmpresaView from "./pages/clientes/[id]";
+import EmpresaEdit from "./pages/clientes/[id]/editar";
 
 function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/" element={<Landing />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/ativar" element={<AtivarConta />} />
@@ -20,12 +21,9 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/empresas/nova" element={<NovaEmpresa />} />
         <Route path="/clientes" element={<Clientes />} />
-        
-        
-
-        {/* 404 */}
+        <Route path="/clientes/:id" element={<EmpresaView />} />
+        <Route path="/clientes/:id/editar" element={<EmpresaEdit />} />
         <Route path="*" element={<div>Página não encontrada</div>} />
-
       </Routes>
     </Router>
   );
